4e4ebb2cb5fe0bb95943e6efba3284d32feac867
Barbara Johnson <johnson.barbara@gmail.com>
2019-05-14 03:16:38 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -4,11 +4,13 @@ class CreateHauntedHouses< ActiveRecord::Migration[4.2]
    create_table :hautned_houses do |t|
       t.string :name
       t.string :location
+      t.string :theme
+      t.float :price
       t.integer :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
-      t.datetime :opening_time
-      t.datetime :closing_time
+      t.datetime :opening_date
+      t.datetime :closing_date
     end
   end
 end
