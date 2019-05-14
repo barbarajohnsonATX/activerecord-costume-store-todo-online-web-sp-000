517ea528e7475d3d6be8300d77bea182e893eec4
Barbara Johnson <johnson.barbara@gmail.com>
2019-05-14 03:04:43 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,8 +13,9 @@ class CreateCostumes< ActiveRecord::Migration[4.2]
    create_table :costumes do |t|
       t.string :name
       t.float :price
-      t.size :string 
-      t.image_url :string
+      t.string :size 
+      t.string :image_url
+      t:timestamps
     end
   end
 end
