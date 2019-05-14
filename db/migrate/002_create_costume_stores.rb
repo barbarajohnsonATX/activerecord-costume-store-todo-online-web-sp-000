f827eb0f1aea664732a746d4246d03996be01e71
Barbara Johnson <johnson.barbara@gmail.com>
2019-05-14 03:09:19 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,10 +4,10 @@ class CreateCostumeStores< ActiveRecord::Migration[4.2]
    create_table :costumes do |t|
       t.string :name
       t.string :location
-      t.integer :costume_inventory:
-      t.string :size 
-      t.string :image_url
-      t.timestamps
+      t.integer :costume_inventory
+      t.integer: :num_of_employees
+      t.boolean :still_in_business
+      t.timestamps :start_time
     end
   end
 end
