2b62fa509779d65be8f0261a1346ac6b072853b6
Barbara Johnson <johnson.barbara@gmail.com>
2019-05-14 02:49:38 +0000

    Automatically backed up by Learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,16 @@
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
+  def up
+  end
+ 
+  def down
+  end
+  
+  def change
+    create_table :students do |t|
+      t.string :name
+    end
+  end
+  
+  
+end
