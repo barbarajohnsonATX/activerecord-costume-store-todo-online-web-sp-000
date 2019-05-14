66707f9ad6a793f1e41ad14944f0511bd5253855
Barbara Johnson <johnson.barbara@gmail.com>
2019-05-14 03:19:35 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,7 +6,7 @@ class CreateHauntedHouses< ActiveRecord::Migration[4.2]
       t.string :location
       t.string :theme
       t.float :price
-      t.string :description
+      t.text :description
        t.boolean :family_friendly
       t.datetime :opening_date
       t.datetime :closing_date
